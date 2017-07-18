e3d562a827518dd68c17efa46774942240622157
xiaoshuang <huiran_shi@126.com>
2017-07-18 14:40:37 +0800

    change settings.py

--- cmdb/settings.py
+++ cmdb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k-=c-&7)7#_an+g99!bgj7x-2_mej*f#a4cod^8*5$+q#iom+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
